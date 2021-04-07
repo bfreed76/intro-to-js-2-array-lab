e29746f801bb13376ecb9b06bc4bf474c836a54d
Brinton <brintonfoyreed@gmail.com>
2021-04-07 14:53:39 -0700

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat= (name) => cats.push(name);
+
+// function destructivelyAppendCat(name) {
+//     cats.push(name);
+// }
+
+const destructivelyPrependCat = (name) => cats.unshift(name);
+
+let destructivelyRemoveLastCat = () => cats.pop();
+
+let destructivelyRemoveFirstCat = () => cats.shift();
+
+let appendCat = (name) => {
+    let newCats = [...cats, name];
+    return newCats;
+}
+
+let prependCat = (name) => { 
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+let removeLastCat = () => {
+    let newCats = cats.slice(0,2);
+    return newCats;
+}
+
+let removeFirstCat = () => {
+    let newCats = cats.slice(1);
+    return newCats;
+}
